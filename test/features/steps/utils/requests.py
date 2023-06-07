11221a9e89afe7c6ec31ed8738be7f896e49cf89
stormeye2000 <info@stormeye.co.uk>
2023-06-07 15:52:00 +0100

    state_get_auction_info feature and steps

--- test/features/steps/utils/requests.py
+++ test/features/steps/utils/requests.py
@@ -14,11 +14,14 @@ class NCTLRequests:
     def get_info_get_validator_changes(self):
         return json.loads(self._request('info_get_validator_changes', '[]'))
 
+    def get_state_get_auction_info(self, hash):
+        return json.loads(self._request('state_get_auction_info', '[{\"Hash\":  \"' + hash + '\"}]'))
+
     def _request(self, method, params) -> str:
 
         url: str = 'http://' + self.config.get_nctl_host() + ':' + self.config.get_nctl_port_rpc() + "/rpc"
-        payload: str = '{"id":"' + str(int(time() * 1000)) + '","jsonrpc":"2.0","method":"' + method + '","params":"' \
-                       + params + '"}'
+        payload: str = '{"id":"' + str(int(time() * 1000)) + '","jsonrpc":"2.0","method":"' + method + '","params":' \
+                       + params + '}'
         headers: dict = {'content-type': 'application/json', 'content-length': str(len(payload))}
 
         return requests.post(url, data=payload, headers=headers).text
