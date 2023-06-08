f4739e3a4c9a423f89ba7cd2ea5f810d74c351d7
stormeye2000 <info@stormeye.co.uk>
2023-06-08 12:32:10 +0100

    state_get_dictionary_item features and steps implemented: not implemented in the sdk

--- sh/docker-init.sh
+++ sh/docker-init.sh
@@ -7,3 +7,4 @@ BASEDIR=$(builtin cd ..; pwd)
  docker cp storm-nctl:/home/casper/casper-node/utils/nctl/assets/net-1/users ${BASEDIR}/assets/net-1
  # copy net-1 chainspec
  docker cp storm-nctl:/home/casper/casper-node/utils/nctl/assets/net-1/chainspec ${BASEDIR}/assets/net-1
+ docker cp storm-nctl:/home/casper/casper-node/utils/nctl/assets/net-1/faucet ${BASEDIR}/assets/net-1
---
+++ test/features/steps/state_get_dictionary_item.py
@@ -0,0 +1,28 @@
+from behave import *
+from pycspr.types import *
+
+from test.features.steps.utils.assets import *
+
+use_step_matcher("re")
+
+# Step definitions for state_get_dictionary_item cucumber tests
+
+
+@given("that the state_get_dictionary_item RCP method is invoked")
+def state_get_dictionary_item_invoked(ctx):
+    print('that the state_get_dictionary_item RCP method is invoked')
+
+    get_faucet_hex_public_key(ctx)
+
+    dictionary_id = DictionaryID_UniqueKey(
+        key='account-hash-' + ctx.sender_key.account_hash.hex()
+    )
+
+    ctx.dictionary_item = ctx.sdk_client.get_dictionary_item(dictionary_id)
+
+    print('The get_dictionary_item fails. Code needs implementing in the DictionaryId')
+
+
+@then("a valid state_get_dictionary_item_result is returned")
+def valid_state_get_dictionary_item_returned(ctx):
+    print('a valid state_get_dictionary_item_result is returned')
