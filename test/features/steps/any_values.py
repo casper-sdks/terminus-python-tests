a809483e597bbf52aeb81bf7072e57cac83691e8
stormeye2000 <carl@stormeye.co.uk>
2024-01-30 15:37:36 +0000

    any values template

--- test/features/steps/any_values.py
+++ test/features/steps/any_values.py
@@ -1,5 +1,4 @@
 from behave import *
-from pycspr import types
 
 from test.features.steps.utils.cl_types_factory import CLTypesFactory
 from test.features.steps.utils.cl_utils import CLTypesUtils
@@ -9,21 +8,28 @@ use_step_matcher("re")
 _factory = CLTypesFactory()
 _utils = CLTypesUtils()
 
+
 @given('an Any value contains a "(.*)" value of "(.*)"')
 def contains_type_and_value(ctx, cltype, val):
     print(f'an Any value contains a {cltype} value of {val}')
 
-    _factory.create_value(types.CL_Any, val)
+    _bytes = _factory.create_value(cltype, val)
+    ctx.cl_any = _factory.create_value('Any', _bytes.value.hex())
 
+    assert ctx.cl_any
+    assert ctx.cl_any.value.hex() == val
 
 @then('the any value\'s bytes are "(.*)"')
 def its_bytes_are(ctx, hex_bytes):
-    print(f'the any value\'s bytes are "{hex_bytes}"')
+    assert ctx.cl_any.value.hex() == hex_bytes
 
 
 @given("that the any value is deployed in a transfer as a named argument")
 def is_deployed(ctx):
     print(f'that the any value is deployed in a transfer as a named argument')
+
+
+
 
 
 @step("the transfer containing the any value is successfully executed")
