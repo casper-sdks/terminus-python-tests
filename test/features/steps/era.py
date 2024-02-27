c8c83e22cdd1c86748ae4b8ae17fb2d4d99fb438
stormeye2000 <carl@stormeye.co.uk>
2024-02-27 11:24:24 +0000

    implement era summary step tests

--- test/features/steps/era.py
+++ test/features/steps/era.py
@@ -1,5 +1,7 @@
 from behave import *
 
+from test.features.steps.utils.validate import validate_merkle_proofs
+
 use_step_matcher("re")
 
 
@@ -7,21 +9,33 @@ use_step_matcher("re")
 def that_era_summary_requested(ctx):
     print('that the era summary is requested via the sdk')
 
-    raise NotImplementedError(u'get_era_summary not implemented')
+    block = ctx.sdk_client.get_block()
+    assert block
+    ctx.block_hash = block['hash']
+
+    ctx.sdk_err_summary = ctx.sdk_client.get_era_summary(ctx.block_hash)
+    assert ctx.sdk_err_summary
 
 @then("request the era summary via the node")
 def era_summary_requested_via_node(ctx):
     print('that the era summary is requested via the sdk')
+
+    ctx.node_err_summary = ctx.node_client.get_era_summary()
+    assert ctx.node_err_summary
 
 
 @step("the block hash of the returned era summary is equal to the block hash of the test node era summary")
 def block_hash_returned_equal(ctx):
     print('the block hash of the returned era summary is equal to the block hash of the test node era summary')
 
+    assert ctx.node_err_summary['era_summary']['block_hash'] == ctx.sdk_err_summary['block_hash']
+
 
 @step("the era of the returned era summary is equal to the era of the returned test node era summary")
 def era_returned_equal(ctx):
     print('the era of the returned era summary is equal to the era of the returned test node era summary')
+
+    assert ctx.node_err_summary['era_summary']['era_id'] == ctx.sdk_err_summary['era_id']
 
 
 @step("the merkle proof of the returned era summary is equal to the merkle proof of the returned test node era summary")
@@ -29,12 +43,16 @@ def merkle_returned_equal(ctx):
     print('the merkle proof of the returned era summary is equal to the merkle proof of the returned test node era '
           'summary')
 
+    validate_merkle_proofs(ctx.node_err_summary['era_summary']['merkle_proof'],
+                           ctx.sdk_err_summary['merkle_proof'])
 
 @step("the state root hash of the returned era summary is equal to the state root hash of the returned test node era "
       "summary")
 def state_root_returned_equal(ctx):
     print('the state root hash of the returned era summary is equal to the state root hash of the returned test node '
           'era summary')
+
+    assert ctx.node_err_summary['era_summary']['state_root_hash'] == ctx.sdk_err_summary['state_root_hash']
 
 
 @step("the delegators data of the returned era summary is equal to the delegators data of the returned test"
@@ -43,8 +61,15 @@ def delegators_equal(ctx):
     print('the delegators data of the returned era summary is equal to the delegators data of the returned test node '
           'era summary')
 
+    assert ctx.node_err_summary['era_summary']['stored_value']['EraInfo']['seigniorage_allocations'] \
+           == ctx.sdk_err_summary['stored_value']['EraInfo']['seigniorage_allocations']
+
+
 @step("the validators data of the returned era summary is equal to the validators data of the returned test node era"
       " summary")
 def validators_equal(ctx):
     print('the validators data of the returned era summary is equal to the validators data of the returned test node '
           'era summary')
+
+    assert ctx.node_err_summary['era_summary']['stored_value']['EraInfo']['seigniorage_allocations'] \
+           == ctx.sdk_err_summary['stored_value']['EraInfo']['seigniorage_allocations']
